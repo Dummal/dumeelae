fc8fba1286bc3855840763a39b0d9bb3e6e4185c
Dummal <soundari1129@gmail.com>
2025-01-30 22:03:10 +0530

    Add Terraform configuration file

--- modules/aws_resources/variables.tf
+++ modules/aws_resources/variables.tf
@@ -1,17 +1,46 @@
-variable "aft_logs_bucket_name" {
-  description = "Name of the S3 bucket for AFT logs"
+variable "s3_bucket_name" {
+  description = "The name of the S3 bucket for storing AFT logs."
   type        = string
 }
 
-variable "aws_region" {
-  description = "The AWS region to deploy resources"
+variable "kms_key_alias" {
+  description = "The alias for the KMS key used for encryption."
   type        = string
-  default     = "us-west-2"
+  default     = "alias/aft-key"
+}
+
+variable "sns_topic_name" {
+  description = "The name of the SNS topic for AFT notifications."
+  type        = string
+  default     = "aft-notifications"
+}
+
+variable "dynamodb_table_name" {
+  description = "The name of the DynamoDB table for handling AFT requests."
+  type        = string
+}
+
+variable "cloudwatch_log_group_name" {
+  description = "The name of the CloudWatch Log Group for AFT operations."
+  type        = string
+}
+
+variable "log_retention_in_days" {
+  description = "The retention period for CloudWatch logs in days."
+  type        = number
+  default     = 90
 }
 
 variable "master_account_id" {
-  description = "The AWS account ID of the master account"
+  description = "The AWS account ID of the master account."
   type        = string
 }
 
-# Add more variables as needed for your specific use case
+variable "tags" {
+  description = "A map of tags to apply to all resources."
+  type        = map(string)
+  default = {
+    Environment = "Production"
+    ManagedBy   = "Terraform"
+  }
+}
