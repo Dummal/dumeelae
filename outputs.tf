f157c60e5ccebfb1fa3397232f787192d2b88b16
Dummal <soundari1129@gmail.com>
2025-02-09 18:26:22 +0530

    Add Terraform configuration file

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 ```hcl
 output "dev_ou_id" {
   value       = aws_organizations_organizational_unit.dev.id
-  description = "The ID of the Development Organizational Unit (OU) in AWS Control Tower."
+  description = "The ID of the Development Organizational Unit (OU) in AWS Organizations."
 }
 
 output "prod_ou_id" {
   value       = aws_organizations_organizational_unit.prod.id
-  description = "The ID of the Production Organizational Unit (OU) in AWS Control Tower."
+  description = "The ID of the Production Organizational Unit (OU) in AWS Organizations."
 }
 
 output "security_ou_id" {
   value       = aws_organizations_organizational_unit.security.id
-  description = "The ID of the Security Organizational Unit (OU) in AWS Control Tower."
+  description = "The ID of the Security Organizational Unit (OU) in AWS Organizations."
 }
 
 output "audit_ou_id" {
   value       = aws_organizations_organizational_unit.audit.id
-  description = "The ID of the Audit Organizational Unit (OU) in AWS Control Tower."
+  description = "The ID of the Audit Organizational Unit (OU) in AWS Organizations."
 }
 ```
