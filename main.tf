a04b7ef7355d259bf49985d26432eace4cfcd64f
Dummal <soundari1129@gmail.com>
2025-02-13 00:06:50 +0530

    Add Terraform configuration file

--- main.tf
+++ main.tf
@@ -51,32 +51,36 @@ resource "aws_iam_group_membership" "marketing_membership" {
 
 resource "aws_iam_policy" "engineering_policy" {
   name        = "EngineeringPolicy"
-  description = "Policy for Engineering group"
-  policy      = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action   = "s3:*"
-        Effect   = "Allow"
-        Resource = "*"
-      }
-    ]
-  })
+  description = "Policy for the Engineering group"
+  policy      = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "s3:*",
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOT
 }
 
 resource "aws_iam_policy" "marketing_policy" {
   name        = "MarketingPolicy"
-  description = "Policy for Marketing group"
-  policy      = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action   = "dynamodb:*"
-        Effect   = "Allow"
-        Resource = "*"
-      }
-    ]
-  })
+  description = "Policy for the Marketing group"
+  policy      = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "dynamodb:*",
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOT
 }
 
 resource "aws_iam_group_policy_attachment" "engineering_policy_attachment" {
--- variables.tf
+++ variables.tf
@@ -26,21 +26,21 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-// Variable for the account alias
-variable "account_alias" {
-  description = "The alias for the AWS account."
-  type        = string
-}
-
 // Variable for the IAM role name
 variable "iam_role_name" {
   description = "The name of the IAM role to be created or managed."
   type        = string
 }
 
-// Variable for the policy ARN to attach to the IAM role
-variable "policy_arn" {
-  description = "The ARN of the policy to attach to the IAM role."
+// Variable for the policy document for the IAM role
+variable "iam_policy_document" {
+  description = "The JSON policy document to attach to the IAM role."
+  type        = string
+}
+
+// Variable for the account alias
+variable "account_alias" {
+  description = "The alias to assign to the AWS account."
   type        = string
 }
 
